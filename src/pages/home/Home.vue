bff59740bf5f0f4e6899b384da8feb6f1d579874
mac <2734756709@qq.com>
2019-06-10 21:20:33 +0800

    实现Header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    hello world
+    <HomeHeader></HomeHeader>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
