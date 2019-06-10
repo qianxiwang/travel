80eba6f27a71f127572c77600a0b390b286bc95d
mac <2734756709@qq.com>
2019-06-10 17:31:51 +0800

    iconfont的引入，安装fastClick

--- src/main.js
+++ src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
+
+// 路由就是根据网址的不同，返回不同的内容给用户
